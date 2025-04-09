cd3bce80937235d890be99b7a9208b4847262466
João <jpnapsp@gmail.com>
2025-04-09 23:24:51 +0100

    Update mcuData.js

--- src/mcuData.js
+++ src/mcuData.js
@@ -1,532 +1,75 @@
 module.exports = [
-  {
-    "title": "Captain America: The First Avenger",
-    "type": "movie",
-    "imdbId": "tt0458339",
-    "releaseYear": "2011"
-  },
-  {
-    "title": "Captain Marvel",
-    "type": "movie",
-    "imdbId": "tt4154664",
-    "releaseYear": "2019"
-  },
-  {
-    "title": "Iron Man",
-    "type": "movie",
-    "imdbId": "tt0371746",
-    "releaseYear": "2008"
-  },
-  {
-    "title": "Iron Man 2",
-    "type": "movie",
-    "imdbId": "tt1228705",
-    "releaseYear": "2010"
-  },
-  {
-    "title": "The Incredible Hulk",
-    "type": "movie",
-    "imdbId": "tt0800080",
-    "releaseYear": "2008"
-  },
-  {
-    "title": "Thor",
-    "type": "movie",
-    "imdbId": "tt0800369",
-    "releaseYear": "2011"
-  },
-  {
-    "title": "The Avengers",
-    "type": "movie",
-    "imdbId": "tt0848228",
-    "releaseYear": "2012"
-  },
-  {
-    "title": "Iron Man 3",
-    "type": "movie",
-    "imdbId": "tt1300854",
-    "releaseYear": "2013"
-  },
-  {
-    "title": "Thor: The Dark World",
-    "type": "movie",
-    "imdbId": "tt1981115",
-    "releaseYear": "2013"
-  },
-  {
-    "title": "Captain America: The Winter Soldier",
-    "type": "movie",
-    "imdbId": "tt1843866",
-    "releaseYear": "2014"
-  },
-  {
-    "title": "Guardians of the Galaxy",
-    "type": "movie",
-    "imdbId": "tt2015381",
-    "releaseYear": "2014"
-  },
-  {
-    "title": "Guardians of the Galaxy Vol. 2",
-    "type": "movie",
-    "imdbId": "tt3896198",
-    "releaseYear": "2017"
-  },
-  {
-    "title": "Avengers: Age of Ultron",
-    "type": "movie",
-    "imdbId": "tt2395427",
-    "releaseYear": "2015"
-  },
-  {
-    "title": "Ant-Man",
-    "type": "movie",
-    "imdbId": "tt0478970",
-    "releaseYear": "2015"
-  },
-  {
-    "title": "Captain America: Civil War",
-    "type": "movie",
-    "imdbId": "tt3498820",
-    "releaseYear": "2016"
-  },
-  {
-    "title": "Black Widow",
-    "type": "movie",
-    "imdbId": "tt3480822",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Spider-Man: Homecoming",
-    "type": "movie",
-    "imdbId": "tt2250912",
-    "releaseYear": "2017"
-  },
-  {
-    "title": "Black Panther",
-    "type": "movie",
-    "imdbId": "tt1825683",
-    "releaseYear": "2018"
-  },
-  {
-    "title": "Doctor Strange",
-    "type": "movie",
-    "imdbId": "tt1211837",
-    "releaseYear": "2016"
-  },
-  {
-    "title": "Thor: Ragnarok",
-    "type": "movie",
-    "imdbId": "tt3501632",
-    "releaseYear": "2017"
-  },
-  {
-    "title": "Avengers: Infinity War",
-    "type": "movie",
-    "imdbId": "tt4154756",
-    "releaseYear": "2018"
-  },
-  {
-    "title": "Ant-Man and the Wasp",
-    "type": "movie",
-    "imdbId": "tt5095030",
-    "releaseYear": "2018"
-  },
-  {
-    "title": "Avengers: Endgame",
-    "type": "movie",
-    "imdbId": "tt4154796",
-    "releaseYear": "2019"
-  },
-  {
-    "title": "Loki",
-    "type": "series",
-    "imdbId": "tt9140554",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "What If...?",
-    "type": "series",
-    "imdbId": "tt10168312",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "WandaVision",
-    "type": "series",
-    "imdbId": "tt9140560",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "The Falcon and the Winter Soldier",
-    "type": "series",
-    "imdbId": "tt9208876",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Shang-Chi and the Legend of the Ten Rings",
-    "type": "movie",
-    "imdbId": "tt9376612",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Spider-Man: Far From Home",
-    "type": "movie",
-    "imdbId": "tt6320628",
-    "releaseYear": "2019"
-  },
-  {
-    "title": "Eternals",
-    "type": "movie",
-    "imdbId": "tt9032400",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Spider-Man: No Way Home",
-    "type": "movie",
-    "imdbId": "tt10872600",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Doctor Strange in the Multiverse of Madness",
-    "type": "movie",
-    "imdbId": "tt9419884",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Hawkeye",
-    "type": "series",
-    "imdbId": "tt10160804",
-    "releaseYear": "2021"
-  },
-  {
-    "title": "Moon Knight",
-    "type": "series",
-    "imdbId": "tt10234724",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Black Panther: Wakanda Forever",
-    "type": "movie",
-    "imdbId": "tt9114286",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Ms. Marvel",
-    "type": "series",
-    "imdbId": "tt10857164",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Thor: Love and Thunder",
-    "type": "movie",
-    "imdbId": "tt10648342",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "She-Hulk: Attorney at Law",
-    "type": "series",
-    "imdbId": "tt10857160",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Werewolf by Night",
-    "type": "movie",
-    "imdbId": "tt15318872",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "The Guardians of the Galaxy Holiday Special",
-    "type": "movie",
-    "imdbId": "tt13623136",
-    "releaseYear": "2022"
-  },
-  {
-    "title": "Ant-Man and the Wasp: Quantumania",
-    "type": "movie",
-    "imdbId": "tt10954600",
-    "releaseYear": "2023"
-  },
-  {
-    "title": "Guardians of the Galaxy Vol. 3",
-    "type": "movie",
-    "imdbId": "tt6791350",
-    "releaseYear": "2023"
-  },
-  {
-    "title": "Secret Invasion",
-    "type": "series",
-    "imdbId": "tt13157618",
-    "releaseYear": "2023"
-  },
-  {
-    "title": "The Marvels",
-    "type": "movie",
-    "imdbId": "tt10676048",
-    "releaseYear": "2023"
-  },
-  {
-    "title": "Echo",
-    "type": "series",
-    "imdbId": "tt13966962",
-    "releaseYear": "2024"
-  },
-  {
-    "title": "Deadpool & Wolverine",
-    "type": "movie",
-    "imdbId": "tt6263850",
-    "releaseYear": "2024"
-  },
-  {
-    "title": "Agatha All Along",
-    "type": "series",
-    "imdbId": "tt15571732",
-    "releaseYear": "2024"
-  },
-  {
-    "title": "Captain America: Brave New World",
-    "type": "movie",
-    "imdbId": "tt14513804",
-    "releaseYear": "2025"
-  },
-  {
-    "title": "Daredevil: Born Again",
-    "type": "series",
-    "imdbId": "tt18923754",
-    "releaseYear": "2025"
-  },
-  {
-    "title": "Thunderbolts*",
-    "type": "movie",
-    "imdbId": "tt20969586",
-    "releaseYear": "2025"
-  },
-  {
-    "title": "The Fantastic: Four First Steps",
-    "type": "movie",
-    "imdbId": "tt10676052",
-    "releaseYear": "2025"
-  },
-  {
-    "title": "Godzilla: King of the Monsters",
-    "type": "movie",
-    "imdbId": "tt3741700",
-    "releaseYear": "2019",
-    "poster": "https://image.tmdb.org/t/p/w500/1lFKupnghulH7oMRTJRLTXMwgow.jpg"
-  },
-  {
-    "title": "Godzilla",
-    "type": "movie",
-    "imdbId": "tt0831387",
-    "releaseYear": "2014",
-    "poster": "https://image.tmdb.org/t/p/w500/zokD6uxR2iWfYM3Y84yGJvnNTK7.jpg"
-  },
-  {
-    "title": "Godzilla vs. Kong",
-    "type": "movie",
-    "imdbId": "tt5034838",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg"
-  },
-  {
-    "title": "Godzilla x Kong: The New Empire",
-    "type": "movie",
-    "imdbId": "tt14539740",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/z1p34vh7dEOnLDmyCrlUVLuoDzd.jpg"
-  },
-  {
-    "title": "A Minecraft Movie",
-    "type": "movie",
-    "imdbId": "tt3566834",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg"
-  },
-  {
-    "title": "In the Lost Lands",
-    "type": "movie",
-    "imdbId": "tt4419684",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/dDlfjR7gllmr8HTeN6rfrYhTdwX.jpg"
-  },
-  {
-    "title": "Fight or Flight",
-    "type": "movie",
-    "imdbId": "tt13652286",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/x4nWnfgJvL045rcUCSJzfgIIY9i.jpg"
-  },
-  {
-    "title": "Peter Pan's Neverland Nightmare",
-    "type": "movie",
-    "imdbId": "tt21955520",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/mOR1Ks0EcXocwMV4EPv4letz0F5.jpg"
-  },
-  {
-    "title": "Black Bag",
-    "type": "movie",
-    "imdbId": "tt30988739",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/hHPovtU4b96LHcoeEwRkGHI5btw.jpg"
-  },
-  {
-    "title": "Snow White",
-    "type": "movie",
-    "imdbId": "tt6208148",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/xWWg47tTfparvjK0WJNX4xL8lW2.jpg"
-  },
-  {
-    "title": "Old Guy",
-    "type": "movie",
-    "imdbId": "tt26442053",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/qx0NV9VEZTXN5c1qruhyHclMauY.jpg"
-  },
-  {
-    "title": "Opus",
-    "type": "movie",
-    "imdbId": "tt29929565",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/m0du9dsiOVeb0SgfqR8ZAEPRxww.jpg"
-  },
-  {
-    "title": "I, the Executioner",
-    "type": "movie",
-    "imdbId": "tt30287778",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/gAtP0usArK5gVOBObnsENKLwML8.jpg"
-  },
-  {
-    "title": "The Monkey",
-    "type": "movie",
-    "imdbId": "tt27714946",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/yYa8Onk9ow7ukcnfp2QWVvjWYel.jpg"
-  },
-  {
-    "title": "Alarum",
-    "type": "movie",
-    "imdbId": "tt31456973",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/v313aUGmMNj6yNveaiQXysBmjVS.jpg"
-  },
-  {
-    "title": "Heretic",
-    "type": "movie",
-    "imdbId": "tt28015403",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/fr96XzlzsONrQrGfdLMiwtQjott.jpg"
-  },
-  {
-    "title": "Paddington in Peru",
-    "type": "movie",
-    "imdbId": "tt5822536",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/1ffZAucqfvQu36x1C49XfOdjuOG.jpg"
-  },
-  {
-    "title": "Pabrik Gula",
-    "type": "movie",
-    "imdbId": "tmdb-1355126",
-    "releaseYear": "2025",
-    "poster": "https://image.tmdb.org/t/p/w500/nwmR5EL5m8tcPBud6OwUyt8p5z1.jpg"
-  },
-  {
-    "title": "Werewolves",
-    "type": "movie",
-    "imdbId": "tt15041836",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/otXBlMPbFBRs6o2Xt6KX59Qw6dL.jpg"
-  },
-  {
-    "title": "Parthenope",
-    "type": "movie",
-    "imdbId": "tt23853982",
-    "releaseYear": "2024",
-    "poster": "https://image.tmdb.org/t/p/w500/5SqICqUQaYFeZs1UnBZ4AN6DwcP.jpg"
-  },
-  {
-    "title": "Ghost Rider",
-    "type": "movie",
-    "imdbId": "tt0259324",
-    "releaseYear": "2007",
-    "poster": "https://image.tmdb.org/t/p/w500/d9Oan9XJJ6gRJa64Ifsgp1sXbC4.jpg"
-  },
-  {
-    "title": "Spider-Man 3",
-    "type": "movie",
-    "imdbId": "tt0413300",
-    "releaseYear": "2007",
-    "poster": "https://image.tmdb.org/t/p/w500/qFmwhVUoUSXjkKRmca5yGDEXBIj.jpg"
-  },
-  {
-    "title": "Jack Kirby: Story Teller",
-    "type": "movie",
-    "imdbId": "tt3001088",
-    "releaseYear": "2007",
-    "poster": "https://image.tmdb.org/t/p/w500/x1yPNPKLJvyiCcoHbR2jE8BuYwy.jpg"
-  },
-  {
-    "title": "Fantastic Four: Rise of the Silver Surfer",
-    "type": "movie",
-    "imdbId": "tt0486576",
-    "releaseYear": "2007",
-    "poster": "https://image.tmdb.org/t/p/w500/f3ldtPF7SESMcyAIyIJHBLlBBkr.jpg"
-  },
-  {
-    "title": "Punisher: War Zone",
-    "type": "movie",
-    "imdbId": "tt0450314",
-    "releaseYear": "2008",
-    "poster": "https://image.tmdb.org/t/p/w500/oOvKJgYUIpfswGHAdW6159bPbvM.jpg"
-  },
-  {
-    "title": "Marvel One-Shot: The Consultant",
-    "type": "movie",
-    "imdbId": "tt2011118",
-    "releaseYear": "2011",
-    "poster": "https://image.tmdb.org/t/p/w500/xqNLXUUvBnfVk6m3QFGGU0Grgs7.jpg"
-  },
-  {
-    "title": "Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer",
-    "type": "movie",
-    "imdbId": "tt2011109",
-    "releaseYear": "2011",
-    "poster": "https://image.tmdb.org/t/p/w500/njrOqsmFH4pxBrhcoslqLfw2OGk.jpg"
-  },
-  {
-    "title": "Marvel One-Shot: Item 47",
-    "type": "movie",
-    "imdbId": "tt2247732",
-    "releaseYear": "2012",
-    "poster": "https://image.tmdb.org/t/p/w500/hnSxG8clwLuAXEkp9emc8HCUcHD.jpg"
-  },
-  {
-    "title": "Mutant X",
-    "type": "series",
-    "imdbId": "tt0283203",
-    "releaseYear": "2001",
-    "poster": "https://image.tmdb.org/t/p/w500/yiKg027jdpkSsguVBI9WqliNWez.jpg"
-  },
-  {
-    "title": "Marvel Studios Legends",
-    "type": "series",
-    "imdbId": "tt13650480",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/EpDuYIK81YtCUT3gH2JDpyj8Qk.jpg"
-  },
-  {
-    "title": "Marvel Studios Assembled",
-    "type": "series",
-    "imdbId": "tt14094206",
-    "releaseYear": "2021",
-    "poster": "https://image.tmdb.org/t/p/w500/v2BHRwtQVkt5fssLdo5MpFgHJPY.jpg"
-  },
-  {
-    "title": "Voices Rising: The Music of Wakanda Forever",
-    "type": "series",
-    "imdbId": "tt26345409",
-    "releaseYear": "2023",
-    "poster": "https://image.tmdb.org/t/p/w500/tD0z0ONzplsO7JLkDk1cMTWR1Rd.jpg"
-  }
+  // 1940s
+  { title: "Captain America: The First Avenger", type: "movie", imdbId: "tt0458339", releaseYear: "2011" },
+
+  // 1990s
+  { title: "Captain Marvel", type: "movie", imdbId: "tt4154664", releaseYear: "2019" },
+
+  // 2010-2012 (Fase 1)
+  { title: "Iron Man", type: "movie", imdbId: "tt0371746", releaseYear: "2008" },
+  { title: "Iron Man 2", type: "movie", imdbId: "tt1228705", releaseYear: "2010" },
+  { title: "The Incredible Hulk", type: "movie", imdbId: "tt0800080", releaseYear: "2008" },
+  { title: "Thor", type: "movie", imdbId: "tt0800369", releaseYear: "2011" },
+  { title: "The Avengers", type: "movie", imdbId: "tt0848228", releaseYear: "2012" },
+
+  // 2013-2017 (Fase 2 e início da Fase 3)
+  { title: "Iron Man 3", type: "movie", imdbId: "tt1300854", releaseYear: "2013" },
+  { title: "Thor: The Dark World", type: "movie", imdbId: "tt1981115", releaseYear: "2013" },
+  { title: "Captain America: The Winter Soldier", type: "movie", imdbId: "tt1843866", releaseYear: "2014" },
+  { title: "Guardians of the Galaxy", type: "movie", imdbId: "tt2015381", releaseYear: "2014" },
+  { title: "Guardians of the Galaxy Vol. 2", type: "movie", imdbId: "tt3896198", releaseYear: "2017" },
+  { title: "Avengers: Age of Ultron", type: "movie", imdbId: "tt2395427", releaseYear: "2015" },
+  { title: "Ant-Man", type: "movie", imdbId: "tt0478970", releaseYear: "2015" },
+  { title: "Captain America: Civil War", type: "movie", imdbId: "tt3498820", releaseYear: "2016" },
+  { title: "Black Widow", type: "movie", imdbId: "tt3480822", releaseYear: "2021" },
+  { title: "Spider-Man: Homecoming", type: "movie", imdbId: "tt2250912", releaseYear: "2017" },
+  { title: "Black Panther", type: "movie", imdbId: "tt1825683", releaseYear: "2018" },
+  { title: "Doctor Strange", type: "movie", imdbId: "tt1211837", releaseYear: "2016" },
+  { title: "Thor: Ragnarok", type: "movie", imdbId: "tt3501632", releaseYear: "2017" },
+
+  // 2017-2023 (Fase 3 e início da Fase 4)
+  { title: "Avengers: Infinity War", type: "movie", imdbId: "tt4154756", releaseYear: "2018" },
+  { title: "Ant-Man and the Wasp", type: "movie", imdbId: "tt5095030", releaseYear: "2018" },
+  { title: "Avengers: Endgame", type: "movie", imdbId: "tt4154796", releaseYear: "2019" },
+  { title: "Spider-Man: Far From Home", type: "movie", imdbId: "tt6322622", releaseYear: "2019" },
+
+  // 2023-2025 (Fase 4 e início da Fase 5)
+  { title: "WandaVision", type: "series", imdbId: "tt9140560", releaseYear: "2021" },
+  { title: "The Falcon and the Winter Soldier", type: "series", imdbId: "tt9208876", releaseYear: "2021" },
+  { title: "Loki Season 1", type: "series", imdbId: "tt9140554", releaseYear: "2021" },
+  { title: "Loki Season 2", type: "series", imdbId: "tt9140554", releaseYear: "2023" },
+  { title: "What If...? Season 1", type: "series", imdbId: "tt10168312", releaseYear: "2021" },
+  { title: "What If...? Season 2", type: "series", imdbId: "tt10168312", releaseYear: "2023" },
+  { title: "What If...? Season 3", type: "series", imdbId: "tt10168312", releaseYear: "2024" },
+  { title: "Shang-Chi and the Legend of the Ten Rings", type: "movie", imdbId: "tt9376612", releaseYear: "2021" },
+  { title: "Eternals", type: "movie", imdbId: "tt9032400", releaseYear: "2021" },
+  { title: "Spider-Man: No Way Home", type: "movie", imdbId: "tt10872600", releaseYear: "2021" },
+  { title: "Doctor Strange in the Multiverse of Madness", type: "movie", imdbId: "tt9419884", releaseYear: "2022" },
+  { title: "Hawkeye", type: "series", imdbId: "tt10160804", releaseYear: "2021" },
+  { title: "Moon Knight", type: "series", imdbId: "tt10234724", releaseYear: "2022" },
+  { title: "Black Panther: Wakanda Forever", type: "movie", imdbId: "tt9114286", releaseYear: "2022" },
+  { title: "Echo", type: "series", imdbId: "tt13966962", releaseYear: "2024" },
+  { title: "She-Hulk: Attorney at Law", type: "series", imdbId: "tt10857160", releaseYear: "2022" },
+  { title: "Ms. Marvel", type: "series", imdbId: "tt10857164", releaseYear: "2022" },
+  { title: "The Guardians of the Galaxy Holiday Special", type: "movie", imdbId: "tt13623136", releaseYear: "2022" },
+  { title: "Werewolf by Night", type: "movie", imdbId: "tt15318872", releaseYear: "2022" },
+  { title: "Secret Invasion", type: "series", imdbId: "tt13157618", releaseYear: "2023" },
+  { title: "Guardians of the Galaxy Vol. 3", type: "movie", imdbId: "tt6791350", releaseYear: "2023" },
+
+  // 2025-2026 (Fase 5)
+  { title: "Ant-Man and the Wasp: Quantumania", type: "movie", imdbId: "tt10954600", releaseYear: "2023" },
+  { title: "The Marvels", type: "movie", imdbId: "tt10676048", releaseYear: "2023" },
+  { title: "Agatha All Along", type: "series", imdbId: "tt15571732", releaseYear: "2024" },
+  { title: "Deadpool & Wolverine", type: "movie", imdbId: "tt6263850", releaseYear: "2024" },
+  { title: "Captain America: Brave New World", type: "movie", imdbId: "tt14513804", releaseYear: "2025" },
+  { title: "Daredevil: Born Again Season 1", type: "series", imdbId: "tt18923754", releaseYear: "2025" },
+
+  // 2026 e Além (Fase 5 e Fase 6)
+  { title: "Thunderbolts*", type: "movie", imdbId: "tt20969586", releaseYear: "2025" },
+  { title: "The Fantastic Four: First Steps", type: "movie", imdbId: "tt10676052", releaseYear: "2025" },
+  { title: "Ironheart", type: "series", imdbId: "tt14916090", releaseYear: "2025" },
+  { title: "Wonder Man", type: "series", imdbId: "tt16302692", releaseYear: "2025" },
+  { title: "Vision Quest", type: "series", imdbId: "tmdb-157306", releaseYear: "2026" },
+  { title: "Avengers: Doomsday", type: "movie", imdbId: "tt12811384", releaseYear: "2026" },
+  { title: "Avengers: Secret Wars", type: "movie", imdbId: "tt12811400", releaseYear: "2027" },
 ];
